a8863d631296415618b24df5ba0a41174a84a427
Ivanio <ivanhuang489@gmail.com>
2022-01-05 20:47:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ const config = require('./config.json');
 client.commands = new Collection();
 client.snipes = new Map();
 
-fs.readdirSync("./commands")
-    .filter(file => file.endsWith(".js"))
-    .forEach(file => {
-        const command = require(`./commands/${file}`)
-        client.commands.set(command.name, command)
-    })
-
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-	for (const file of eventFiles) {
-		const event = require(`./events/${file}`);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+
+	client.commands.set(command.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args, client));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	}
-}    
+}
 
 client.login(config.token);
